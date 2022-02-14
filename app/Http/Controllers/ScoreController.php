be9b8e276d489e43e70544befe1c3ca9188b0190
Pintu Maniya <pintumaniya91@gmail.com>
2022-02-15 00:30:54 +0530

    completed scorecard api

---
+++ app/Http/Controllers/ScoreController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScoreController extends Controller
+{
+    protected $url = 'match/';
+
+    public function getScoreboard(Request $request){
+        $tokenObj = new TokenGenerateController();
+        $token = $tokenObj->checkToken();
+        $result = $this->prepareScoreboardData($token, $request);
+        return response()->success($result, "ScoreBoard get succssfully");
+    }
+
+    public function prepareScoreBoardData($token, $request) {
+        $apiResult = sendRequest($token, $this->url.$request->match_key.'/');
+        $result = [];
+        try {
+            $key = "";
+            foreach($apiResult['teams'] as $team_key => $value) {
+                if(in_array($request->team_key, $value)) {
+                    $key = $team_key;
+                }
+            }
+            $result['run'] = $apiResult['teams'][$key]['code'].' '.$apiResult['play']['innings'][$key.'_1']['score_str'];
+            $result['toss_status'] = $apiResult['teams'][$apiResult['toss']['winner']]['code'] . ' Won toss & '.$apiResult['toss']['elected'];
+            foreach ($apiResult['squad'][$key]['player_keys'] as $player_key => $row) {
+                $result[$row] =  $apiResult['players'][$row]['score']['1']['batting'];
+            }
+        }catch (\Exception $e){
+            dd($e->getMessage());
+            report($e);
+        }
+        return $result;
+    }
+}
